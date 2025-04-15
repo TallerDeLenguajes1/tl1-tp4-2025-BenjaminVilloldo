78f325eb2925b99d1afc857c7220c599aae1679d
BenjaminVilloldo <benjaminvillol@gmail.com>
2025-04-15 15:20:59 -0300

    En proceso

--- ejercicio.c
+++ ejercicio.c
@@ -10,10 +10,29 @@ struct Tarea{
 struct Nodo{
     Tarea T;
     Nodo *Siguiente;
-};
+}typedef Nodo;
+
+Nodo * crearListaVacia(){
+    return NULL;
+}
+
+Nodo * crearNodo( int dato){
+    Nodo * NNodo= (*Nodo) malloc(sizeof(Tarea));
+    NNodo->Siguiente = NULL;
+    return NNodo;
+}
 
 
+
+void cargaTareas(){
+    printf
+    ("Ingrese el id de la tarea");
+
+}
 int main(){
-    
+
+ 
+//start = crearListaVacia();
+
     return 0;
 }
